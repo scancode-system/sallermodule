ae96a5ccd595279afcdd67790e3c48585e7355e3
Your Name <you@example.com>
2019-12-25 04:05:55 -0300

    .

--- Database/Migrations/2019_10_30_211003_create_sallers_table.php
+++ Database/Migrations/2019_10_30_211003_create_sallers_table.php
@@ -17,6 +17,7 @@ class CreateSallersTable extends Migration
            $table->bigIncrements('id');
            $table->string('name')->unique();
            $table->string('email')->unique()->nullable();
+           $table->decimal('goal', 10, 2)->default(0);
            $table->timestamp('email_verified_at')->nullable();
            $table->string('password');
            
