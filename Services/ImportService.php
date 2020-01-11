9cd800ac6153e0b04aca67ce431f250ae45c8be9
Your Name <you@example.com>
2020-01-11 14:34:22 -0300

    .

--- Services/ImportService.php
+++ Services/ImportService.php
@@ -4,12 +4,15 @@ namespace Modules\Saller\Services;
 
 use Modules\Saller\Imports\SallersImport;
 use Maatwebsite\Excel\Facades\Excel;
+use Illuminate\Support\Facades\Storage;
 
 class ImportService {
 
 	public function import($path)
 	{
-		Excel::import(new SallersImport, $path);
+		if(Storage::exists($path)){
+			Excel::import(new SallersImport, $path);
+		}
 	}
 
 }
